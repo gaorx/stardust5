e521f6ec82a3e2378f4143fc8fd34a4c8312700c
gaorx <rongxin.gao@gmail.com>
2023-09-02 23:44:14 +0800

    x

--- sdecho/menu.go
+++ sdecho/menu.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gaorx/stardust5/sdslices"
 	"github.com/gaorx/stardust5/sdstrings"
 	"github.com/labstack/echo/v4"
+	"github.com/samber/lo"
+	"slices"
 )
 
 type Menu struct {
@@ -19,9 +21,10 @@ type MenuItem struct {
 	Icon     string      `json:"icon,omitempty"`
 	Children []*MenuItem `json:"children,omitempty"`
 	Object   Object      `json:"-"`
+	Tags     []string    `json:"-"`
 }
 
-func MenuReify(ctx context.Context, ec echo.Context, menuId string) *Menu {
+func MenuReify(ctx context.Context, ec echo.Context, menuId string, tags []string) *Menu {
 	menus := MustGet[Menus](ec, keyMenus)
 	for _, menu := range menus {
 		if menu != nil && menu.Id == menuId {
@@ -29,25 +32,31 @@ func MenuReify(ctx context.Context, ec echo.Context, menuId string) *Menu {
 			if err != nil {
 				return &Menu{Id: menuId, Items: []*MenuItem{}}
 			}
-			return menu.Reify(ctx, ec, token)
+			return menu.Reify(ctx, ec, token, tags)
 		}
 	}
 	return nil
 }
 
-func (menu *Menu) Reify(ctx context.Context, ec echo.Context, token Token) *Menu {
+func (menu *Menu) Reify(ctx context.Context, ec echo.Context, token Token, tags []string) *Menu {
 	return &Menu{
 		Id:    menu.Id,
-		Items: sdslices.Ensure(reifyMenuItems(ctx, ec, token, menu.Items)),
+		Items: sdslices.Ensure(reifyMenuItems(ctx, ec, token, tags, menu.Items)),
 	}
 }
 
-func (item *MenuItem) Reify(ctx context.Context, ec echo.Context, token Token) *MenuItem {
+func (item *MenuItem) Reify(ctx context.Context, ec echo.Context, token Token, tags []string) *MenuItem {
 	// children
-	filteredChildren := reifyMenuItems(ctx, ec, token, item.Children)
-	if len(filteredChildren) <= 0 {
+	reifiedChildren := reifyMenuItems(ctx, ec, token, tags, item.Children)
+	if len(reifiedChildren) <= 0 {
 		err := AccessControlCheck(ctx, ec, token, item.Object, ActionShow)
 		if err != nil {
+			return nil
+		}
+	}
+	if len(item.Tags) > 0 {
+		// 如果菜单项本身有item.Tags，说明它可以通过tags来过滤
+		if len(lo.Intersect(tags, item.Tags)) <= 0 {
 			return nil
 		}
 	}
@@ -58,15 +67,16 @@ func (item *MenuItem) Reify(ctx context.Context, ec echo.Context, token Token) *
 		Href:     sdstrings.ExpandShellLike(item.Href, mapper),
 		Icon:     item.Icon,
 		Object:   item.Object,
-		Children: sdslices.Ensure(filteredChildren),
+		Children: sdslices.Ensure(reifiedChildren),
+		Tags:     slices.Clone(item.Tags),
 	}
 }
 
-func reifyMenuItems(ctx context.Context, ec echo.Context, token Token, items []*MenuItem) []*MenuItem {
+func reifyMenuItems(ctx context.Context, ec echo.Context, token Token, tags []string, items []*MenuItem) []*MenuItem {
 	var filteredItems []*MenuItem
 	for _, item := range items {
 		if item != nil {
-			if filteredItem := item.Reify(ctx, ec, token); filteredItem != nil {
+			if filteredItem := item.Reify(ctx, ec, token, tags); filteredItem != nil {
 				filteredItems = append(filteredItems, filteredItem)
 			}
 		}
