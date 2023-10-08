cfb00cc30fccf71703de250809c9d09b96549835
gaorx <rongxin.gao@gmail.com>
2023-10-08 11:59:20 +0800

    fix: sderr中加入了Scatter用于还原Combine中组合的错误信息

--- sderr/misc.go
+++ sderr/misc.go
@@ -9,3 +9,13 @@ func Combine(errs []error) error {
 	}
 	return merged
 }
+
+func Scatter(err error) []error {
+	if err == nil {
+		return nil
+	}
+	if err1, ok := AsT[*MultipleError](err); ok && err1 != nil {
+		return err1.Errors
+	}
+	return []error{err}
+}
